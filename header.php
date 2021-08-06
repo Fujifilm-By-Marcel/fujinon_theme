67ade4618802740ad9c75c63a2c3b7e886171614
marcel.munevar.contractor <marcel.munevar.contractor@fujifilm.com>
2021-08-06 10:17:01 -0400

    google font and error reporting

--- header.php
+++ header.php
@@ -8,7 +8,9 @@
  *
  * @package fujinon_theme
  */
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
@@ -16,7 +18,13 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 
+	<!-- GOOGLE FONTS -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700;800&display=swap" rel="stylesheet">
+	
 	<?php wp_head(); ?>
 </head>
 
