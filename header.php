d74babd52fa3927b295e6382ed9cc2e54760646b
marcel.munevar.contractor <marcel.munevar.contractor@fujifilm.com>
2021-08-20 17:18:43 -0400

    added urbanist font

--- header.php
+++ header.php
@@ -23,7 +23,7 @@ error_reporting(E_ALL);
 	<!-- GOOGLE FONTS -->
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700;800&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700;800&family=Urbanist:wght@900&display=swap" rel="stylesheet">
 	
 	<?php wp_head(); ?>
 </head>
