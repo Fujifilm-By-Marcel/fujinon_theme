7bbc364e8cb22e8f120c73cc791864377df18a8f
marcel.munevar.contractor <marcel.munevar.contractor@fujifilm.com>
2021-09-01 16:58:01 -0400

    new page

---
+++ page-our-brand.php
@@ -0,0 +1,119 @@
+<?php
+function enqueue_scripts(){
+	wp_enqueue_script('owl-carousel', get_template_directory_uri().'/js/owl.carousel/owl.carousel.min.js', array( 'jquery' ), false, true ); 
+	wp_enqueue_style('owl-carousel', get_template_directory_uri().'/js/owl.carousel/assets/owl.carousel.min.css');
+	wp_enqueue_style('owl-carousel-theme', get_template_directory_uri().'/js/owl.carousel/assets/owl.theme.default.min.css');
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_scripts' );
+get_header();
+?>
+<style>
+
+</style>
+
+<section class="video standard-spacing-margin">	
+	<div class="video-container">
+		<img src="<?php the_field('hero_bg'); ?>" style="width:100%;">
+	</div>
+	<div class="hero-overlay">
+		<div class="container">
+			<div style="max-width:55ch;">
+				<?php the_field('hero_overlay_text'); ?>
+			</div>
+			<div>
+				
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section class="history standard-spacing-margin">
+	<div class="container header-block" style="">
+		<?php the_field('carousel_header_text') ?>
+	</div>
+	<div class="timeline-carousel">
+		<div class="container-inner">
+			<?php if( have_rows('carousel') ): ?>
+			    <div class="owl-carousel owl-theme">
+			    <?php while( have_rows('carousel') ): the_row(); 
+			        $image = get_sub_field('image');
+			        ?>
+			        <div data-dot='<span></span><p><?php the_sub_field('year'); ?></p>'>
+			            <div class="split" style="max-width:70em;margin:auto;">			         
+				            <div>
+					            <h3 class="underline"><?php the_sub_field('year'); ?></h3>
+					            <p><?php the_sub_field('text'); ?></p>
+					        </div>
+					        <div>
+					            <?php echo wp_get_attachment_image( $image, 'full' ); ?>
+					        </div>
+					    </div>
+			        </div>
+			    <?php endwhile; ?>
+			    </div>
+			<?php endif; ?>
+		</div>
+	</div>
+	<!--<div style="position: relative;">
+		<div style="background: #000;width: 1px;height: 109px;left: 50%;position: absolute;transform: translateX(-50%);top: -86px;z-index: 10;"></div>
+	</div>-->
+</section>
+
+
+<?php do_shortcode('[my_discover_block]'); ?>
+
+<section class="teasers brand-teasers standard-spacing-padding black-bg">
+	<?php
+	if( have_rows('products') ):
+	    while( have_rows('products') ) : the_row(); $image = get_sub_field('image'); ?>
+
+	        
+	        <div class="split">
+	        	<div>
+	        		<div class="container">
+	        			<?php the_sub_field('text'); ?>		
+	        		</div>	        			
+	        	</div>	        	
+	        	<div><img src="<?php echo $image['url'] ?>" alt="" width="<?php echo $image['width'] ?>" height="<?php echo $image['height'] ?>"></div>
+	        </div>
+	    
+	    <?php
+	    endwhile;
+	endif;
+	?>
+</section>
+
+
+<script>
+	jQuery(document).ready(function( $ ) {
+
+		function centerDots(){
+			let carouselwidth = $('.owl-dots').width();
+			let activedot = $('.owl-dots .owl-dot.active');
+			let activeleft = activedot.position().left;	
+			let dotwidth = activedot.width();	
+			let offset = carouselwidth/2-activeleft-dotwidth/2;
+			$('.owl-dots').css('left', offset+"px");
+		}
+
+		let owl  = $(".owl-carousel");
+		owl.owlCarousel({
+			items:1,
+			dotsData: true,
+			autoplay:true,
+			autoplayHoverPause:true,
+			loop:true,
+		});
+
+		owl.on('changed.owl.carousel', function(event) {			
+			centerDots();
+		})
+
+		owl.trigger('refresh.owl.carousel');
+
+	});
+</script>
+<?php
+get_sidebar();
+get_footer();
