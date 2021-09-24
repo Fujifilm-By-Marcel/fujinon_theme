dc697d301d5d33563875b02c074a6e7cd1a94e33
marcel.munevar.contractor <marcel.munevar.contractor@fujifilm.com>
2021-09-24 10:34:30 -0400

    curl operation for inriver

--- page-products.php
+++ page-products.php
@@ -24,6 +24,61 @@ get_template_part( 'template-parts/content', 'hero' );
 ?>
 
 
+<?php
+
+// create & initialize a curl session
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_POST, 1);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($curl,CURLOPT_HTTPHEADER, array (	
+	  'X-inRiver-APIKey: ea45402db538119b33cdd314c8c39834',
+    'Accept: application/json',
+    'Content-Type: application/json',
+));
+
+
+//get list of entities
+curl_setopt($curl, CURLOPT_URL, "https://apiuse.productmarketingcloud.com/api/v1.0.0/query");
+curl_setopt($curl, CURLOPT_POSTFIELDS, '{ 
+   "dataCriteria": [ 
+     { 
+       "fieldTypeId": "ProductSubCategory1", 
+       "value": [ 
+         "Lenses"  
+       ], 
+       "operator": "ContainsAny" 
+     } 
+   ],  
+   "dataCriteria": [ 
+     { 
+       "fieldTypeId": "ProductSubCategory2", 
+       "value": "MK Zoom Lens", 
+       "operator": "Equal" 
+     } 
+   ], 
+ }');
+$entities = json_encode(json_decode(curl_exec($curl), true)['entityIds']);
+
+
+//get data from entities
+curl_setopt($curl, CURLOPT_URL, "https://apiuse.productmarketingcloud.com/api/v1.0.0/entities:fetchdata");
+curl_setopt($curl, CURLOPT_POSTFIELDS, '{
+  "entityIds": '.$entities.',
+  "objects": "EntitySummary,Media"
+}');
+$data = json_decode(curl_exec($curl));
+
+//close curl
+curl_close($curl);
+
+
+echo "<pre>";
+print_r($data);
+echo "</pre>";
+
+?>
+
+<img src="https://asset.productmarketingcloud.com/api/assetstorage/2179_a07da038-6f11-4132-8b6a-23fd5b8a544d">
 
 <?php
 get_sidebar();
