8c8699531d45629440a0db6f7a3778fd37d209f0
marcel.munevar.contractor <marcel.munevar.contractor@fujifilm.com>
2021-10-08 10:50:36 -0400

    added cover class

--- template-parts/content-hero-mini.php
+++ template-parts/content-hero-mini.php
@@ -1,6 +1,6 @@
 <section class="hero mini-hero <?php echo get_query_var('hero-classes'); ?>">	
 	<div class="hero-container">		
-		<?php echo wp_get_attachment_image( get_field('hero_bg'), 'full', "", array( "class" => "full-width" ) );  ?>
+		<?php echo wp_get_attachment_image( get_field('hero_bg'), 'full', "", array( "class" => "cover" ) );  ?>
 	</div>
 	<div class="hero-overlay">
 		<div class="container">			
