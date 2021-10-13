2cc717753383f59179146a7c358e611b31f7332c
marcel.munevar.contractor <marcel.munevar.contractor@fujifilm.com>
2021-10-13 16:34:58 -0400

    download and register media files

--- data/refresh-data.php
+++ data/refresh-data.php
@@ -1,5 +1,7 @@
 <?php 
+//delete this file from remote host when not in use
 require_once("../../../../wp-load.php");
+require_once( ABSPATH . 'wp-admin/includes/post.php' );
 do_action('buildProductData');
 echo "action done.<br>";
 echo 'check the <a href="logs.txt">log file</a><br>';
