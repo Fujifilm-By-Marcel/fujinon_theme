ccee454759d79510890f0820e49356a4a9d2c092
marcel.munevar.contractor <marcel.munevar.contractor@fujifilm.com>
2021-10-19 18:54:38 -0400

    taxonomy.php

--- data/refresh-data.php
+++ data/refresh-data.php
@@ -2,6 +2,7 @@
 //delete this file from remote host when not in use
 require_once("../../../../wp-load.php");
 require_once( ABSPATH . 'wp-admin/includes/post.php' );
+require_once( ABSPATH . '/wp-admin/includes/taxonomy.php');
 do_action('buildProductData');
 echo "action done.<br>";
 echo 'check the <a href="logs.txt">log file</a><br>';
