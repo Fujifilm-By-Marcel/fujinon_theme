13d295cf4bbddefcb857c06836ff8a2361355bfa
marcel.munevar.contractor <marcel.munevar.contractor@fujifilm.com>
2021-10-20 17:33:14 -0400

    product filters

--- page-products.php
+++ page-products.php
@@ -6,25 +6,112 @@ function enqueue_scripts(){
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_scripts' );
 get_header();
-$list = $inriver->getProductData();
+
+//get top level categories
+$args = array(
+   'taxonomy' => 'inriver_categories',
+   'orderby' => 'name',
+   'order'   => 'ASC',
+   'parent' => '0',
+);
+
+$top_cats = get_categories($args);
+
+function echo_filters($value){	
+	
+	//save parent id for use in loop
+	$parent_id = $value->term_id;
+
+	//query based on term_id of parent
+	$query_args = array(
+	   'taxonomy' => 'inriver_categories',
+	   'orderby' => 'name',
+	   'order'   => 'ASC',
+	   'parent' => $parent_id,
+	);
+	$child_cats = get_categories($query_args);
+
+	//if there are query results results
+	if( count($child_cats) ){
+
+		//echo "<pre>";
+		//print_r($child_cats);
+		//echo "</pre>";
+
+		echo '<div class="filter" data-index="',$parent_id,'">';
+		foreach($child_cats as $value){						
+			echo '<a href="#" class="button" data-index="', $value->term_id, '">', $value->name, '</a>';
+		} 
+		echo '</div>';
+	}
+
+	foreach($child_cats as $value){
+		echo_filters($value);
+	}
+}
 ?>
-<section class="product-nav">
+<section id="product-nav">
 	<div class="container">
 		<div class="split">
-			<?php $i=0;foreach($list as $value){ ?>
-			<div  class="underline nav-item <?php echo ($i ? "" : "active"); ?>"><a href="#"><h3><span><?php echo $value->name ?></span></h3></a></div>			
-			<?php $i++;} ?>
+			<?php foreach($top_cats as $key => $value){ ?>
+			<div  class="underline nav-item <?php echo ($key ? "" : "active"); ?>"><a class='category-switcher' href="#" data-index='<?php echo $value->term_id ?>' ><h3><span><?php echo $value->name ?></span></h3></a></div>			
+			<?php } ?>
 		</div>
 	</div>
 </section>
 <?php 
 //echo hero
 set_query_var( 'hero-classes', 'standard-spacing-margin' );
-get_template_part( 'template-parts/content', 'hero' ); 
+get_template_part( 'template-parts/content', 'hero' );
 ?>
 
 
+<?php foreach($top_cats as $key => $value){ ?>
+	<section class="product-category" data-index=<?php echo $value->term_id ?> style="<?php echo $key ? 'display:none;' : "" ?>">
+		<div class="container">
+			<div class="container header-block" style="">
+				<!-- todo:get field from victor -->
+			</div>
+			<div class="product-filters">
+				<?php				
+				echo_filters($value);
+				?>			
+			</div>
+			<div class="products">
+				<?php //get_template_part( 'products/content', 'products', array('data'=>$value) ); ?>
+			</div>
+		</div>
+	</section>
+<?php } ?>
+<script>
+(function($) {
+	//onclick for category
 
+	$(".category-switcher").click(function (){
+		var index = $(this).data('index');
+		$(".category-switcher").parent().removeClass('active');
+		$(this).parent().addClass('active');
+		$(".product-category").hide();
+		$(".product-category[data-index="+index+"]").show();
+		return false;
+	});
+
+
+	//onclick for filters
+	$('.filter .button').click(function (){
+		var filter = $(this).parent();
+		var index = $(this).data('index');
+		var parentIndex = filter.data('index');
+		var parentFilter = $( ".button[data-index="+parentIndex+"]").parent();
+		//console.log(".button[data-index="+parentIndex+"]")//.parent();
+		var categoryindex = $(this).closest('.product-category').data('index');
+		$(".product-category[data-index="+categoryindex+"] .filter").not(filter).not(parentFilter).hide();
+		$(".product-category[data-index="+categoryindex+"] .filter[data-index="+index+"]").show();
+		return false;
+	});
+
+})( jQuery );
+</script>
 <?php
 get_sidebar();
 get_footer();
