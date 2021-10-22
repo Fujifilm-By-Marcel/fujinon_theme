13a5bacc5715113b53f330f292d3957a0c69c2c3
marcel.munevar.contractor <marcel.munevar.contractor@fujifilm.com>
2021-10-22 16:49:05 -0400

    allowed post id for custom fields

--- template-parts/content-hero.php
+++ template-parts/content-hero.php
@@ -1,16 +1,20 @@
-<section class="hero <?php echo get_query_var('hero-classes'); ?>">	
+<?php 
+$id = get_query_var ( 'hero-id' ,get_the_id()); 
+$style = get_query_var ( "style" );
+?>
+<section class="hero <?php echo get_query_var('hero-classes'); ?>" style="<?php echo $style ?>">	
 	<div class="hero-container">
-		<?php the_field('hero_video'); ?>
-		<?php echo wp_get_attachment_image( get_field('hero_bg'), 'full', "", array( "class" => "cover" ) );  ?>
+		<?php the_field('hero_video', $id); ?>
+		<?php echo wp_get_attachment_image( get_field('hero_bg', $id), 'full', "", array( "class" => "cover" ) );  ?>
 	</div>
 	<div class="hero-overlay">
 		<div class="container">
 			<div class="left-half">				
-				<img src="<?php echo get_field('hero_logo')['url']; ?>" alt="" width="<?php echo get_field('hero_logo')['width']; ?>" height="<?php echo get_field('hero_logo')['height']; ?>">
-				<?php the_field('hero_overlay_text'); ?>
+				<img src="<?php echo get_field('hero_logo', $id)['url']; ?>" alt="" width="<?php echo get_field('hero_logo', $id)['width']; ?>" height="<?php echo get_field('hero_logo', $id)['height']; ?>">
+				<?php the_field('hero_text', $id); ?>
 			</div>
 			<div class="right-half">
-				<p><?php the_field('hero_credit'); ?></p>
+				<p><?php the_field('hero_credit', $id); ?></p>
 			</div>
 		</div>
 	</div>
