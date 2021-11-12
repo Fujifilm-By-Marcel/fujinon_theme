0eabdd49a713c30a36993afb7028437a542b88e9
marcel.munevar.contractor <marcel.munevar.contractor@fujifilm.com>
2021-11-12 16:25:22 -0500

    swap sections

--- page-home.php
+++ page-home.php
@@ -7,7 +7,7 @@ set_query_var( 'hero-classes', '' );
 get_template_part( 'template-parts/content', 'hero' ); 
 ?>
 
-<section class="home-tiles standard-spacing-margin" style="color:white;">
+<section class="home-tiles" style="color:white;">
 	<?php
 	if( have_rows('tiles') ):
 	    while( have_rows('tiles') ) : the_row();?>
@@ -33,26 +33,24 @@ get_template_part( 'template-parts/content', 'hero' );
 	endif;
 	?>
 </section>
-<section class="teasers standard-spacing-margin">
-	<div class="container" style="max-width:80em;">
-		<?php
-		if( have_rows('teasers') ):
-		    while( have_rows('teasers') ) : the_row(); ?>
-		    	<div class="split">
-		    		<div>
-		    			<div>
-		    				<?php the_sub_field('text'); ?>
-		    			</div>
-		    		</div>
-		    		<div>
-		    			<img src="<?php echo get_sub_field('image')['url']; ?>" width="<?php echo get_sub_field('image')['width']; ?>" height="<?php echo get_sub_field('image')['height']; ?>" style="margin:auto;">
-		    		</div>
-		    	</div>
-		    <?php
-		    endwhile;
-		endif;
-		?>
-	</div>
+<section class="teasers standard-spacing-margin black-bg" style="padding:3em 0;">
+	<?php
+	if( have_rows('teasers') ):
+	    while( have_rows('teasers') ) : the_row(); ?>
+	    	<div class="split column-reverse">
+	    		<div>
+	    			<div>
+	    				<?php the_sub_field('text'); ?>
+	    			</div>
+	    		</div>
+	    		<div>
+	    			<?php echo wp_get_attachment_image( get_sub_field('image'), 'full' ); ?>	
+	    		</div>
+	    	</div>
+	    <?php
+	    endwhile;
+	endif;
+	?>
 </section>
 <?php 
 get_template_part( 'template-parts/content', 'discover-block' ); 
