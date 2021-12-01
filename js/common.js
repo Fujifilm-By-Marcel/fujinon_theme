e2425ea24572c145c9b3b9168881aa7b0a4df590
marcel.munevar.contractor <marcel.munevar.contractor@fujifilm.com>
2021-12-01 15:36:57 -0500

    newsletter

--- js/common.js
+++ js/common.js
@@ -2,10 +2,13 @@
 	$('.modal .close').click(function(){
 		$(this).closest('.modal').hide();
 	});
-
 	$('.modal').click(function(event){
 		if ( $(event.target).hasClass('modal') ) {
 			$(this).hide();
 		}		
+	});
+	$('.open-newsletter-modal').click(function(){
+		$('.newsletter-modal').show();
+		return false;
 	});
 })( jQuery );
