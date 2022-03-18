bd358f7622674727afbcd3c05a574115c5458a0c
marcel.munevar.contractor <marcel.munevar.contractor@fujifilm.com>
2022-03-18 18:07:26 -0400

    clean db function

---
+++ data/test-clean-database.php
@@ -0,0 +1,6 @@
+<?php 
+//delete this file from remote host when not in use
+//require_once("../../../../wp-load.php");
+//require_once( ABSPATH . 'wp-admin/includes/post.php' );
+//require_once( ABSPATH . '/wp-admin/includes/taxonomy.php');
+//do_action('cleanDatabase');
