b357f7fc323f7e1662b089b92620d7b64ee1384e
marcel.munevar.contractor <marcel.munevar.contractor@fujifilm.com>
2022-04-22 16:35:10 -0400

    fixed background image

--- template-parts/content-discover-block.php
+++ template-parts/content-discover-block.php
@@ -1,6 +1,12 @@
-<?php if( get_field("discover_block", "option")['content'] != "" ){ ?>
+<?php if( get_field("discover_block", "option")['content'] != "" ){ 
+	if(get_field("discover_block", "option")['background']){
+		$background = "background:url(".get_field("discover_block", "option")['background'].");";
+	} else {
+		$background = "";
+	}
+	?>
 <section class="discover-block standard-spacing-margin" >
-	<div class='container' style="background:#acacac;background:<?php echo get_field("discover_block", "option")['background'] ?>;" style="max-width:90em;">
+	<div class='container' style="background:#acacac;<?php echo $background ?>;" style="max-width:90em;">
 		<div style="max-width:<?php echo get_field('discover_block', "option")['max_width']; ?>;margin:auto;">
 			<?php echo get_field("discover_block", "option")['content'] ?>
 		</div>
