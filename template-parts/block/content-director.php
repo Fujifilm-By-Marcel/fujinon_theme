5f932b404239c22fbd3173cccc98246d47e235f0
marcel.munevar.contractor <marcel.munevar.contractor@fujifilm.com>
2022-10-07 15:15:34 -0400

    various styles for blog posts

--- template-parts/block/content-director.php
+++ template-parts/block/content-director.php
@@ -15,34 +15,36 @@ echo "<div id=".$id." class='director-single-block ".$classname." '>";
 
 ?>
 <div class="director-block" >
-    <img class="portrait" src="<?php echo get_field('portrait', $director_pid) ?>" alt="">
-    <p class="director-title"><?php echo get_field('title', $director_pid) ?></p>
-    <h4 class="director-name"><?php echo get_the_title($director_pid) ?></h4>
-    <p class="blurb"><?php echo get_field('blurb', $director_pid) ?></p>
-    <div class="social">
-        <?php
+    <div class="content">
+        <img class="portrait" src="<?php echo get_field('portrait', $director_pid) ?>" alt="">
+        <p class="director-title"><?php echo get_field('title', $director_pid) ?></p>
+        <h4 class="director-name"><?php echo get_the_title($director_pid) ?></h4>
+        <p class="blurb"><?php echo get_field('blurb', $director_pid) ?></p>
+        <div class="social">
+            <?php
 
-        // Check rows existexists.
-        if( have_rows('social', $director_pid) ):
+            // Check rows existexists.
+            if( have_rows('social', $director_pid) ):
 
-            // Loop through rows.
-            while( have_rows('social', $director_pid) ) : the_row();
+                // Loop through rows.
+                while( have_rows('social', $director_pid) ) : the_row();
 
-                // Load sub field value.
-                $fa_icon = get_sub_field('fa_icon', $director_pid);
-                $link = get_sub_field('link', $director_pid);
+                    // Load sub field value.
+                    $fa_icon = get_sub_field('fa_icon', $director_pid);
+                    $link = get_sub_field('link', $director_pid);
+                    // Do something...
+                    echo "<a target='_blank' href='".$link."' class='social-icon' > ".$fa_icon."</a>";
+
+                // End loop.
+                endwhile;
+
+            // No value.
+            else :
                 // Do something...
-                echo "<a target='_blank' href='".$link."' class='social-icon' > ".$fa_icon."</a>";
+            endif;
 
-            // End loop.
-            endwhile;
-
-        // No value.
-        else :
-            // Do something...
-        endif;
-
-        ?>
+            ?>
+        </div>
     </div>
 </div>
 
